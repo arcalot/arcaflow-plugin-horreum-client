331d30eaf0225fb4f8a9934f6b56d26936400a41
Dustin Black <dblack@redhat.com>
2023-11-20 12:42:40 +0100

    split schema

--- arcaflow_plugin_horreum_client/horreum_client_plugin.py
+++ arcaflow_plugin_horreum_client/horreum_client_plugin.py
@@ -5,36 +5,10 @@ import typing
 from dataclasses import dataclass
 from arcaflow_plugin_sdk import plugin, validation
 from horreum_client_schema import (
-    InputParams
+    InputParams,
+    SuccessOutput,
+    ErrorOutput
 )
-
-
-@dataclass
-class InputParams:
-    """
-    This is the data structure for the input parameters of the step defined
-    below.
-    """
-
-    name: typing.Annotated[str, validation.min(1)]
-
-
-@dataclass
-class SuccessOutput:
-    """
-    This is the output data structure for the success case.
-    """
-
-    message: str
-
-
-@dataclass
-class ErrorOutput:
-    """
-    This is the output data structure in the error  case.
-    """
-
-    error: str
 
 
 @plugin.step(
