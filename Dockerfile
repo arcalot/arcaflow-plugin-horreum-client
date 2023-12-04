71d02ed8797b907bc6f8881c6c3bd00888af3748
Dustin Black <dblack@redhat.com>
2023-12-04 19:43:15 +0100

    add responses mock and enable functional tests

--- Dockerfile
+++ Dockerfile
@@ -20,9 +20,9 @@ COPY tests /app/${package}/tests
 ENV PYTHONPATH /app/${package}
 WORKDIR /app/${package}
 
-# # # Run tests and return coverage analysis
-# # RUN python -m coverage run tests/test_${package}.py \
-# #  && python -m coverage html -d /htmlcov --omit=/usr/local/*
+# Run tests and return coverage analysis
+RUN python -m coverage run tests/test_${package}.py \
+ && python -m coverage html -d /htmlcov --omit=/usr/local/*
 
 
 # STAGE 2 -- Build final plugin image
@@ -30,7 +30,7 @@ FROM quay.io/arcalot/arcaflow-plugin-baseimage-python-osbase:0.3.1@sha256:0e9384
 ARG package
 
 COPY --from=build /app/requirements.txt /app/
-# # COPY --from=build /htmlcov /htmlcov/
+COPY --from=build /htmlcov /htmlcov/
 COPY LICENSE /app/
 COPY README.md /app/
 COPY ${package}/ /app/${package}
--- tests/test_arcaflow_plugin_horreum_client.py
+++ tests/test_arcaflow_plugin_horreum_client.py
@@ -2,17 +2,20 @@
 
 import unittest
 import horreum_client_plugin
+import horreum_client_schema
 
 from arcaflow_plugin_sdk import plugin
 
+
 plugin_input = horreum_client_plugin.InputParams(
+    mock=True,
     horreum_url="https://horreum.foo.bar.com",
     horreum_keycloak_url="https://horreum-keycloak.foo.bar.com",
     horreum_username="username",
     horreum_password="supersecret",
     test_name="$.test_name",
     test_owner="ownername",
-    test_access_rights="PUBLIC",
+    test_access_rights=horreum_client_schema.access.PUBLIC,
     test_start_time="$.start_time",
     test_stop_time="$.end_time",
     data_object={
@@ -22,7 +25,6 @@ plugin_input = horreum_client_plugin.InputParams(
         "uuid": "ffffffff-0264-43dc-97f4-42792e8bad6b",
         "start_time": "2023-11-16T15:07:36.345179+01:00",
         "end_time": "2023-11-16T15:16:43.960644+01:00",
-        "test_type": "boot-time",
         "test_results": [
             {
                 "lorem": {
@@ -55,7 +57,7 @@ class HorreumClientTest(unittest.TestCase):
         plugin.test_object_serialization(plugin_input)
 
         plugin.test_object_serialization(
-            horreum_client_plugin.SuccessOutput(horreum_run_id="12345")
+            horreum_client_plugin.SuccessOutput(horreum_run_id=12345)
         )
 
         plugin.test_object_serialization(
@@ -72,7 +74,7 @@ class HorreumClientTest(unittest.TestCase):
         self.assertEqual("success", output_id)
         self.assertEqual(
             output_data,
-            horreum_client_plugin.SuccessOutput("Hello, Example Joe!"),
+            horreum_client_plugin.SuccessOutput(12345),
         )
 
 
